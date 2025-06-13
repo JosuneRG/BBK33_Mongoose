33792bf9babb7a5163974310afc8fbcd8d85b0d1
JosuneRG <josunerg94@gmail.com>
2025-06-13 19:08:28 +0200

    comentarios añadidos al codigo

--- routes/product.js
+++ routes/product.js
@@ -3,27 +3,27 @@ const router = express.Router()
 const ProductController = require('../controllers/productController')
 
 //Crear un nuevo producto
-//http://localhost:3000/products
+//http://localhost:3001/products
 router.post('/', ProductController.create)
 
 //Obtener todos los productos
-//http://localhost:3000/products
+//http://localhost:3001/products
 router.get('/', ProductController.getAll)
 
 //Obtener un producto por su ID
-//http://localhost:3000/products/id/123abc
+//http://localhost:3001/products/id/123abc
 router.get('/id/:_id', ProductController.getById)
 
 //Buscar productos por nombre (parcial)
-//http://localhost:3000/products/name/Laptop
+//http://localhost:3001/products/name/Laptop
 router.get('/name/:name', ProductController.getProductsByName)
 
 //Eliminar un producto por ID
-// http://localhost:3000/products/123abc
+// http://localhost:3001/products/123abc
 router.delete('/:_id', ProductController.delete)
 
 //Actualizar un producto por ID
-//http://localhost:3000/products/123abc
+//http://localhost:3001/products/123abc
 router.put('/:_id', ProductController.update)
 
 module.exports = router
